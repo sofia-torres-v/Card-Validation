0598436c5c69e708ae7cbf6fe00d3f747caa116b
sofiauldemiaclear <sofy.torresv@gmail.com>
2023-09-26 22:49:13 -0500

    ADD:animation to rotate the credit card image

--- src/index.js
+++ src/index.js
@@ -1,67 +1,29 @@
-import validator from './validator.js';
-// Declarando  mis variables
-const nombre = document.getElementById('nombre');
-const cardNumber = document.getElementById('cardNumber');
-const btn = document.getElementById('btn');
-const texto = document.getElementById('texto');
-const texto2 = document.getElementById('texto2');
-const imagen = document.getElementById('imagen');
-const mascaraContent = document.getElementById('contenedorMascara');
-const agregar = document.getElementById('textoMascara');
+// import validator from "../js/validator.js";
 
+const tarjeta = document.querySelector('#tarjeta');
+const menuIcon = document.querySelector('.menu-icon');
+const elementosMenu = document.getElementById('menu');
 
-// Declarando mi evento
-btn.addEventListener('click', validate);
-
-
-//Declarando mi funcion y sus condiciones
-function validate() {
-  const valorNombre = nombre.value;
-  const valorNumero = cardNumber.value;
-
-  //Aqui insertamos la funciones que vamos a llamar desde validator.js
-  const luhm = validator.isValid(valorNumero);
-  const mostrarMascara  = validator.maskify(valorNumero);
-
-
-  //CONDICION PARA INPUT NOMBRE
-  if (valorNombre === '') {   
-    texto2.innerHTML = ' Este campo es obligatorio'
-    nombre.style.border = '1px solid red'       //llama a nombre para utilizar el imput mas no el valor del input
-      
-  } else {
-    texto2.innerHTML = '';
-    nombre.style.border = '1px solid green'  
-  }
-
-
-  //CONDICION PARA EL INPUT TARJETA
-  if (valorNumero === '') {
-    texto.innerHTML = ' Este campo es obligatorio'
-    cardNumber.style.border = '1px solid red'
-      
-  }else if(luhm) { //Aqui validamos si cumple el algoritmo 
-    texto.innerHTML = 'Tarjeta válida'
-    cardNumber.style.border = '1px solid green'
-
-
-  }else if (luhm === false && valorNumero.length < 5 ) {
-    texto.innerHTML = ' Error de validacion'
-    cardNumber.style.border = '1px solid red'
-    agregar.innerHTML = valorNumero;  // mostrando  texto de mascara sin# porque es < a5 
-
-  } else {
-    texto.innerHTML = ' Error de validacion'
-    cardNumber.style.border = '1px solid red'  //cualquier otro caso
-  }
+tarjeta.addEventListener('click', () =>{
+  tarjeta.classList.toggle('active');
  
-  if (valorNombre !=='' && valorNumero !== '') {
-    mostrarMascara;
-    agregar.innerHTML = mostrarMascara;
-    imagen.style.display = 'none';
-    mascaraContent.style.display = 'grid';
-  } 
+});
+
+menuIcon.addEventListener('click', showMenu);
+
+// function showMenu() {
+//   if (elementosMenu.classList.contains('active')) {
+//     elementosMenu.classList.remove('active')
+//   } else {
+//     elementosMenu.classList.add('active')
+//   }
+// }
+
+
+// Si tiene o no  la clase aparece y desaparece 
+function showMenu() {
+  elementosMenu.classList.toggle('active')
 }
 
 
-// console.log(validator);
+
