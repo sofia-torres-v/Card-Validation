bb75782c5118cc4ce4f227451272a2a221077dd9
sofiauldemiaclear <sofy.torresv@gmail.com>
2023-09-27 00:15:02 -0500

    ADD: Select of the month dynamically generated with javascript

--- src/index.js
+++ src/index.js
@@ -3,27 +3,32 @@
 const tarjeta = document.querySelector('#tarjeta');
 const menuIcon = document.querySelector('.menu-icon');
 const elementosMenu = document.getElementById('menu');
+const formulario = document.querySelector('#formulario-tarjeta');
 
+// Rotación de la tarjeta
 tarjeta.addEventListener('click', () =>{
   tarjeta.classList.toggle('active');
- 
 });
 
+
+// Menú hamburguesa
 menuIcon.addEventListener('click', showMenu);
-
-// function showMenu() {
-//   if (elementosMenu.classList.contains('active')) {
-//     elementosMenu.classList.remove('active')
-//   } else {
-//     elementosMenu.classList.add('active')
-//   }
-// }
-
-
-// Si tiene o no  la clase aparece y desaparece 
 function showMenu() {
+  // Si tiene o no  la clase aparece y desaparece 
   elementosMenu.classList.toggle('active')
 }
+
+
+// Select del mes generado dinamicamnete
+for (let i = 1; i <= 12; i++){
+  const opcion = document.createElement('option');
+  opcion.value = i;
+  opcion.innerHTML = i;
+  formulario.selectMes.appendChild(opcion)
+
+}
+
+
 
 
 
