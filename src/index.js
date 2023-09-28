17b8db2f52042d361bf146a85222692b72758569
sofiauldemiaclear <sofy.torresv@gmail.com>
2023-09-28 15:40:40 -0500

    ADD:function to display the card number on the corresponding image

--- src/index.js
+++ src/index.js
@@ -1,46 +1,54 @@
 // import validator from "../js/validator.js";
 
-const tarjeta = document.querySelector('#tarjeta');
-const menuIcon = document.querySelector('.menu-icon');
-const elementosMenu = document.getElementById('menu');
-const formulario = document.querySelector('#formulario-tarjeta');
-
+const tarjeta = document.querySelector("#tarjeta");
+const menuIcon = document.querySelector(".menu-icon");
+const elementosMenu = document.getElementById("menu");
+const formulario = document.querySelector("#formulario-tarjeta");
+const numeroTarjeta = document.querySelector("#tarjeta .numero");
+// const nombreTarjeta = document.querySelector("#tarjeta .nombre");
 // Rotación de la tarjeta
-tarjeta.addEventListener('click', () =>{
-  tarjeta.classList.toggle('active');
+tarjeta.addEventListener("click", () => {
+  tarjeta.classList.toggle("active");
 });
 
-
 // Menú hamburguesa
-menuIcon.addEventListener('click', showMenu);
+menuIcon.addEventListener("click", showMenu);
 function showMenu() {
-  // Si tiene o no  la clase aparece y desaparece 
-  elementosMenu.classList.toggle('active')
+  // Si tiene o no  la clase aparece y desaparece
+  elementosMenu.classList.toggle("active");
 }
 
-
 // Select del mes generado dinámicamnete
-for (let i = 1; i <= 12; i++){
-  const opcion = document.createElement('option');
+for (let i = 1; i <= 12; i++) {
+  const opcion = document.createElement("option");
   opcion.value = i;
   opcion.innerHTML = i;
-  formulario.selectMes.appendChild(opcion)
+  formulario.selectMes.appendChild(opcion);
 }
 
 // Select del año generado dinámicamnete
 const yearActual = new Date().getFullYear();
 for (let i = yearActual; i <= yearActual + 8; i++) {
-  const opcion = document.createElement('option');
+  const opcion = document.createElement("option");
   opcion.value = i;
   opcion.innerHTML = i;
   formulario.selectYear.appendChild(opcion);
 }
 
 // Capturando el valo del input numero de tarjeta
-formulario.inputNumero.addEventListener('keyup', (e) => {
+formulario.inputNumero.addEventListener("keyup", (e) => {
   const valorInput = e.target.value;
 
   // Eliminamos espacios en blanco/letras/colocamos espacios cada 4 digítos
-  formulario.inputNumero.value = valorInput.replace(/\s/g, '').replace(/\D/g, '').replace(/([0-9]{4})/g, '$1 ' ).trin();
-}); 
+  formulario.inputNumero.value = valorInput
+    .replace(/\s/g, "")
+    .replace(/\D/g, "")
+    .replace(/([0-9]{4})/g, "$1 ")
+    .trim();
 
+  numeroTarjeta.textContent = valorInput;
+
+  if (valorInput === "") {
+    numeroTarjeta.textContent = "#### #### #### ####";
+  }
+});
